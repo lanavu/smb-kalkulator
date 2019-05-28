71669d8fda4914d08c0aae91ebf1b15bbf0adb51
Lana Vu <anhlv@ifi.uio.no>
2019-05-28 22:44:37 +0200

    Init

---
+++ src/components/InputField.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="input-field">
+    <label :for="id">{{ label }}</label>
+    <input :type="inputType" :id="id" />
+  </div>
+</template>
+
+<script>
+import { generateUUID } from "@/helpers/functions";
+
+export default {
+  name: "InputField",
+  props: {
+    inputType: {
+      type: String,
+      default: "text"
+    },
+    label: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      id: null
+    };
+  },
+  created() {
+    this.id = this.generateUUID();
+  },
+  methods: {
+    generateUUID
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.input-field {
+  width: 450px;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+
+  input {
+    padding: 15px;
+  }
+}
+</style>
---
+++ src/views/SMB.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>SMB</h1>
+    <InputField inputType="number" label="Input tall" />
+    <InputField label="Input tekst" />
+    <InputField label="Input tekst" />
+    <InputField label="Input tekst" />
+    <InputField label="Input tekst" />
+  </div>
+</template>
+
+<script>
+import InputField from "@/components/InputField";
+export default {
+  name: "SMB",
+  components: {
+    InputField
+  }
+};
+</script>
+
+<style></style>

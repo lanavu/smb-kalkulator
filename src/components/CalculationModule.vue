b2fc5d3b349a19f3fcb8d63dbca7f8246b2a2a66
Lana Vu <anhlv@ifi.uio.no>
2019-06-05 19:45:34 +0200

    Calculation module

---
+++ src/components/CalculationModule.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="input-info">
+    <p class="info-row">
+      <span>Boligen din koster</span>
+      <span>{{ toLocaleString(userInput.propertyValue) }} kr</span>
+    </p>
+    <p class="info-row">
+      <span>Du eier</span>
+      <span>{{ toLocaleString(userInput.ownershipShare) }}%</span>
+    </p>
+    <p class="info-row">
+      <span>Samlet gjeld på boligen</span>
+      <span>{{ toLocaleString(userInput.propertyDept) }} kr</span>
+    </p>
+  </div>
+</template>
+
+<script>
+import { toLocaleString } from "@/helpers/functions";
+import { mapState } from "vuex";
+export default {
+  name: "CalculationModule",
+  computed: {
+    ...mapState({
+      userInput: state => state.userInput
+    })
+  },
+  methods: {
+    toLocaleString
+  }
+};
+</script>
+
+<style>
+.info-row {
+  display: flex;
+  justify-content: space-between;
+  font-size: 1.2em;
+  border-bottom: 1px solid lightgrey;
+  padding: 15px 0;
+}
+</style>
--- src/views/SMB.vue
+++ src/views/SMB.vue
@@ -1,26 +1,13 @@
 <template>
   <div class="SMB">
-    <div class="SMB-borad md-layout">
-      <div class="user-input md-layout-item">
+    <div class="md-layout md-gutter md-alignment-center-center">
+      <div class="user-input md-layout-item md-size-40 md-small-size-100">
         <h1 class="md-display-2 light-type">Kjøp ut andre</h1>
         <p>Regn ut hvor mye du må betale for å kjøpe ut andre eiere</p>
         <UserInputForm />
       </div>
-      <div class="result md-layout-item">
-        <div class="input-info">
-          <h3>Boligen din koster</h3>
-          <p class="md-display-1">
-            {{ toLocaleString(userInput.propertyValue) }} kr
-          </p>
-          <h3>Du eier</h3>
-          <p class="md-display-1">
-            {{ toLocaleString(userInput.ownershipShare) }}%
-          </p>
-          <h3>Samlet gjeld på boligen</h3>
-          <p class="md-display-1">
-            {{ toLocaleString(userInput.propertyDept) }} kr
-          </p>
-        </div>
+      <div class="result md-layout-item md-size-40 md-small-size-100">
+        <CalculationModule />
       </div>
     </div>
   </div>
@@ -28,15 +15,13 @@
 
 <script>
 import { mapState } from "vuex";
-import { toLocaleString } from "@/helpers/functions";
 import UserInputForm from "@/components/UserInputForm";
+import CalculationModule from "@/components/CalculationModule";
 export default {
   name: "SMB",
   components: {
-    UserInputForm
-  },
-  methods: {
-    toLocaleString
+    UserInputForm,
+    CalculationModule
   },
   computed: {
     ...mapState({
@@ -47,19 +32,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~vue-material/dist/theme/engine";
+
 .SMB {
-  max-width: 1024px;
   width: 100%;
+  max-width: 1280px;
 }
 .result {
-  display: flex;
-  justify-content: center;
-  background: lightgray;
-  margin: 15px;
-  padding: 15px;
-
-  .input-info {
-    text-align: center;
-  }
+  background: md-get-palette-color(bluegrey, 50);
 }
 </style>
